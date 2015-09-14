026b513a6c857844c7352e32c462805a55a7540a
paulhoulston <paul.houlston@hotmail.co.uk>
2015-09-14 10:50:38 +0100

    Renamed tests to better reflect the first test case

--- IniSerializer/Class1.cs
+++ IniSerializer/Class1.cs
@@ -2,16 +2,16 @@
 
 namespace IniSerializer
 {
-    public class Given_I_want_to_serialize_an_object_to_an_ini_file
+    public class Given_I_want_to_serialize_an_object_with_no_values_to_an_ini_file
     {
         class When_the_object_is_serialized
         {
             [Test]
-            public void Then_the_section_name_is_output_correctly()
+            public void Then_the_section_name_is_output_on_the_first_line()
             {
                 var objToSerialize = new ObjectToSerialize();
                 var serializer = new IniSerializer();
-                Assert.AreEqual("[Test Section Heading]", serializer.Serialize(objToSerialize));
+                Assert.AreEqual("[Test Section Heading]", serializer.Serialize(objToSerialize).Split('\r', '\n')[0]);
             }
         }
     }
