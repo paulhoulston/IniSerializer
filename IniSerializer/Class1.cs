bd397aaa8aa03b59c24ef2ce25b649e4aaf30c43
paulhoulston <paul.houlston@hotmail.co.uk>
2015-09-14 10:45:13 +0100

    Added first failing test to check that section name is serialized in the ini output when serializing an object

---
+++ IniSerializer/Class1.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace IniSerializer
+{
+    public class Given_I_want_to_serialize_an_object_to_an_ini_file
+    {
+        class When_the_object_is_serialized
+        {
+            [Test]
+            public void Then_the_section_name_is_output_correctly()
+            {
+                Assert.AreEqual("[Test Section Heading]", serializedObject);
+            }
+        }
+    }
+}
